750358de71d90e41b04b9929c076ee0d1bb3e314
Johannes Herold <herold.johannes@gmail.com>
2021-01-19 10:24:20 +0100

    Ausgabe des Updateskripts angepasst

--- html/cgi-bin/update.sh
+++ html/cgi-bin/update.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
 # output HTTP header
-echo "Content-Type: text/plain"
+echo "Content-Type: text/plain; charset=utf-8"
 echo ""
 
 # switch to repo directory
-cd /var/www/html
+#cd /var/www/html
 
 # discard all changes to avoid "you have unstaged changes" error
-git reset --hard
+git reset --hard > /dev/null
 
 # store old revision hash
-OLD=$(git rev-parse HEAD)
+OLD_HASH=$(git rev-parse HEAD)
 
 # fetch new version from server
-git pull --no-rebase
+git pull --no-rebase > /dev/null
+
+# get new hash to compare to old one
+NEW_HASH=$(git rev-parse HEAD)
 
 # show all commit messages since stored revision hash
-git --no-pager log --pretty=format:'* %s' $OLD..
+echo "Änderungen seit letztem Update:"
+git --no-pager log --pretty=format:'* %s' $OLD_HASH..
+if [[ "$OLD_HASH" == "$NEW_HASH" ]]
+then
+	echo "Keine Änderungen gefunden."
+fi
+	
