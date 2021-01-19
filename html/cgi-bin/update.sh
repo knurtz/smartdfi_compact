f9f00743d9608b4e5abf91a9fb590913c19837f3
Johannes Herold <herold.johannes@gmail.com>
2021-01-19 11:12:48 +0100

    Updateskript (vermutlich) fertig...

--- html/cgi-bin/update.sh
+++ html/cgi-bin/update.sh
@@ -3,27 +3,35 @@
 # output HTTP header
 echo "Content-Type: text/plain; charset=utf-8"
 echo ""
+echo "Aktualisiere..."
 
 # switch to repo directory
-cd /var/www/html
+if [ -z "$LOCAL_TEST" ]
+then
+	cd /var/www/html
+fi
+
+# log update to file
+echo "" >> ./html/cgi-bin/update-log.txt
+echo "Updating repo on $(date)" >> ./html/cgi-bin/update-log.txt
 
 # discard all changes to avoid "you have unstaged changes" error
-git reset --hard > /dev/null
+if [ -z "$LOCAL_TEST" ]
+then
+	git reset --hard > /dev/null 2>&1
+fi
 
 # store old revision hash
 OLD_HASH=$(git rev-parse HEAD)
 
-# fetch new version from server
-git pull --no-rebase > /dev/null
-
-# get new hash to compare to old one
-NEW_HASH=$(git rev-parse HEAD)
+# fetch new version from server, log to file
+git pull --no-rebase >> ./html/cgi-bin/update-log.txt 2>&1
 
 # show all commit messages since stored revision hash
-echo "Änderungen seit letztem Update:"
-git --no-pager log --pretty=format:'* %s' $OLD_HASH..
-if [[ "$OLD_HASH" == "$NEW_HASH" ]]
+if [[ "$OLD_HASH" == "$(git rev-parse HEAD)" ]]
 then
 	echo "Keine Änderungen gefunden."
+else
+	echo "Änderungen seit letztem Update:"
+git --no-pager log --pretty=format:'* %s' $OLD_HASH..
 fi
-	
