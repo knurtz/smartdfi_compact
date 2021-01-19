8dbe33b8fcfbe1906a97f9b3939c1881409b34e6
Johannes Herold <herold.johannes@gmail.com>
2021-01-19 10:10:27 +0100

    Updateskript bissl pimped

--- html/cgi-bin/update.sh
+++ html/cgi-bin/update.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# output HTTP header
 echo "Content-Type: text/plain"
 echo ""
 
+# switch to repo directory
 cd /var/www/html
+
+# discard all changes to avoid "you have unstaged changes" error
 git reset --hard
-git pull
+
+# store old revision hash
+$OLD=$(git rev-parse HEAD)
+
+# fetch new version from server
+git pull --no-rebase
+
+# show all commit messages since stored revision hash
+git --no-pager log --pretty=format:'* %s' $OLD..
