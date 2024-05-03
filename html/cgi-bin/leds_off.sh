132c674038ab9904c17a4c4a83d6cb405fc3f941
SmartDFI <dfi@smartdfi>
2024-05-03 09:44:54 +0200

    Added scripts for LEDs

---
+++ html/cgi-bin/leds_off.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+raspi-gpio set 23 op dl
+raspi-gpio set 24 op dl
+raspi-gpio set 25 op dl
+
+echo Success
---
+++ html/cgi-bin/leds_on.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+raspi-gpio set 23 op dh
+raspi-gpio set 24 op dh
+raspi-gpio set 25 op dh
+
+echo Success
